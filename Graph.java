2e1314818f4fa1bee3f84eca93ae5bf4758e999f
Rachel Holman <78620569+rachel-holman@users.noreply.github.com>
2022-04-25 15:20:47 -0400

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,4 +1,25 @@
 
 public class Graph {
 
+  public Graph(String filename) {
+		
+	}
+	
+	public static boolean useDistCost() {
+		return true;
+		
+	}
+	
+	public static boolean returnAddress() {
+		return true;
+		
+	}
+	
+	@Override
+	public String toString() {
+		String ret = "";
+		return ret;
+		
+	}
+  
 }
