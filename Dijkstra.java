d83848f582d3732aa75a9a3acf35e0d6598896f4
Rachel Holman <78620569+rachel-holman@users.noreply.github.com>
2022-04-25 15:24:31 -0400

    Update Dijkstra.java

--- Dijkstra.java
+++ Dijkstra.java
@@ -1,4 +1,14 @@
 
 public class Dijkstra {
-
+  
+  public static Path shortestPath() {
+		Path p = null;
+		return p;
+	}
+	
+	public static int totalCost() {
+		int total = 0;
+		return total;
+	}
+  
 }
