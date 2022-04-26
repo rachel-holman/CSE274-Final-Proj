eeeadfe1ca4c5323df2fadcd800e84727ae41015
ryanjamesyu <104389599+ryanjamesyu@users.noreply.github.com>
2022-04-26 15:23:38 -0400

    Update Path.java

--- Path.java
+++ Path.java
@@ -1,4 +1,16 @@
 
-public class Path {
-
+public class Path implements Comparable<Path> {
+  private char vertex;
+  private String path;
+  private int cost;
+  
+  public Path(String vertex, int cost, String pathStr) {
+    this.vertex = vertex;
+    this.cost = cost;
+    this.pathStr = pathStr;
+  }
+  
+  public int compareTo(Path other) {
+    return cost - other.cost;    // lower cost goes first
+  }
 }
