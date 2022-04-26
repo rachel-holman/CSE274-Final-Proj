7cce62b9ebeff54153ca898a748823fa5f6af34e
ryanjamesyu <104389599+ryanjamesyu@users.noreply.github.com>
2022-04-26 15:52:19 -0400

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -2,11 +2,20 @@
 public class Graph {
 	
 	private HashMap<Vertex, TreeSet> graph;
-	public static boolean useDiseCost;
+	public static boolean useDiseCo	st;
 	public static boolean returnAddress;
 
-  public Graph(String filename) {
-		
+	public Graph(String filename) {
+		Scanner readFile = new Scanner(filename);
+		readFile.nextLine();
+		readFile.nextLine();	// skip first two rows in the file
+		String line = readFile.nextLine();
+		while(!(line.equals("<\Node">")) {
+		    	graph.put(new Vertex(line), new TreeSet<Edge>());	// not sure if the the set should contain edges or not
+			String line = readFile.nextLine();
+		}
+	    	
+   		// add new while loop for adding edges to TreeSet
 	}
 	
 	@Override
