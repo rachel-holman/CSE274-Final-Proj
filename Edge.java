9c9ce417d393ab778350a453ec47f95c0860427c
ryanjamesyu <104389599+ryanjamesyu@users.noreply.github.com>
2022-04-26 16:00:46 -0400

    Update Edge.java

--- Edge.java
+++ Edge.java
@@ -2,4 +2,11 @@
 public class Edge {
   private Vertex fromVertex, toVertex;
   private int timeCost, distanceCost;
+  
+  public Edge(String line) {
+    String parts = line.split("\t");
+    // how to get access to vertices?
+    this.timeCost = Integer.parseInt(parts[2]);
+    this.distanceCost = Integer.parseInt(parts[3]);
+  }
 }
