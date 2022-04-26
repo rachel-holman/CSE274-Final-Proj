affc0bbca5af700fdbe26454e126ec2962a300c2
ryanjamesyu <104389599+ryanjamesyu@users.noreply.github.com>
2022-04-26 16:28:52 -0400

    Update Edge.java

--- Edge.java
+++ Edge.java
@@ -3,9 +3,10 @@ public class Edge {
   private Vertex fromVertex, toVertex;
   private int timeCost, distanceCost;
   
-  public Edge(String line) {
+  public Edge(String line, Graph graph) {   // honestly don't know how I feel about this constructor
     String parts = line.split("\t");
-    // how to get access to vertices?
+    this.fromVertex = graph.getVertex(parts[0]);
+    this.toVertex = graph.getVertex(parts[1]);
     this.timeCost = Integer.parseInt(parts[2]);
     this.distanceCost = Integer.parseInt(parts[3]);
   }
