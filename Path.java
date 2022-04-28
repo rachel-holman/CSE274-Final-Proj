146f01f59bee4ca51ce7f48b6da7b33f3a90144c
ryanjamesyu <104389599+ryanjamesyu@users.noreply.github.com>
2022-04-28 16:33:40 -0400

    Update Path.java

--- Path.java
+++ Path.java
@@ -1,27 +1,53 @@
-public class Path implements Comparable<Path> {
-	private String vertex;
-	private String pathStr;
-	private int cost;
+import java.util.LinkedList;
 
-	public Path(String vertex, int cost, String pathStr) {
+public class Path implements Comparable<Path> {
+	private Vertex vertex;
+	private int cost;
+	private LinkedList<Edge> edges;
+
+	public Path(Vertex vertex, int cost, LinkedList<Edge> edges) {
 		this.vertex = vertex;
 		this.cost = cost;
-		this.pathStr = pathStr;
+		this.edges = edges;
 	}
 
 	public int compareTo(Path other) {
 		return cost - other.cost;    // lower cost goes first
 	}
 
-	public String getVertex() {
+	public Vertex getVertex() {
 		return this.vertex; 
 	}
 
-	public String getPathStr() {
-		return this.pathStr; 
+	public LinkedList<Edge> getEdges() {
+		return this.edges; 
 	}
 
 	public int getCost() {
 		return this.cost; 
 	}
+	
+	@Override
+	public String toString() {
+		if(Graph.returnAddress) return addressToString();
+		String ret = "";
+		for(Edge e : edges) {
+			ret += e.getFromVertex().getSymbol() + "->" + e.getToVertex().getSymbol();
+			ret += "\tCost: " + (Graph.useDistCost ? e.getDistanceCost() + " miles" : e.getTimeCost() + " min");
+			ret += "\n";
+		}
+		ret += "\tTotal cost: " + getCost() + (Graph.useDistCost ? " miles" : " min");
+		return ret;
+	}
+
+	private String addressToString() {
+		String ret = "";
+		for(Edge e : edges) {
+			ret += e.getFromVertex().getAddress() + "->" + e.getToVertex().getAddress();
+			ret += "\tCost: " + (Graph.useDistCost ? e.getDistanceCost() + " miles" : e.getTimeCost() + " min");
+		}
+		ret += "\tTotal cost: " + getCost() + (Graph.useDistCost ? " miles" : " min");
+		return ret;
+	}
+
 }
