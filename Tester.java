7c32795a64347bb132a31a8459061d447d309b3f
ryanjamesyu <104389599+ryanjamesyu@users.noreply.github.com>
2022-04-28 16:32:03 -0400

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,6 +1,9 @@
 
 public class Tester {
-  public static void main(String[] args) {
-    // Do stuff here
-  }
+	public static void main(String[] args) {
+//		new Window_GPS(new Graph("MapInformationXY.txt"));
+		Graph g = new Graph("MapInformation.txt");
+		// System.out.println(g);
+		System.out.println(Dijkstra.shortestPath(g, g.getVertex("A"), g.getVertex("B")));
+	}
 }
