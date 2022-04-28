6d35083b090cd25a5b7fbcf6401ea77dab119d92
ryanjamesyu <104389599+ryanjamesyu@users.noreply.github.com>
2022-04-28 10:42:58 -0400

    Update Edge.java

--- Edge.java
+++ Edge.java
@@ -3,12 +3,11 @@ public class Edge {
   private Vertex fromVertex, toVertex;
   private int timeCost, distanceCost;
   
-  public Edge(String line, Graph graph) {   // honestly don't know how I feel about this constructor (mainly the getVertex method in the Graph class)
-    String parts = line.split("\t");
-    this.fromVertex = graph.getVertex(parts[0]);
-    this.toVertex = graph.getVertex(parts[1]);
-    this.timeCost = Integer.parseInt(parts[2]);
-    this.distanceCost = Integer.parseInt(parts[3]);
+  public Edge(Vertex fromVertex, Vertex toVertex, int timeCost, int distanceCost) {
+    this.fromVertex = fromVertex;
+    this.toVertex = toVertex;
+    this.timeCost = timeCost;
+    this.distanceCost = distanceCost;
   }
   
   public Vertex getFromVertex() {
