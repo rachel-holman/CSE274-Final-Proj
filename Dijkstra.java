3ee32c82cdd7914d7f92f008d8609eb4b2cd32b9
ryanjamesyu <104389599+ryanjamesyu@users.noreply.github.com>
2022-04-28 11:04:57 -0400

    Update Dijkstra.java

--- Dijkstra.java
+++ Dijkstra.java
@@ -1,38 +1,40 @@
+import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 public class Dijkstra {
-	
-	private statiic int totalCost;
-	
+
+	private static int totalCost;
+
 	// definitely not confident that this method actually works
 	public static Path shortestPath(Graph graph, Vertex start, Vertex end) {
 
 		PriorityQueue<Path> pq = new PriorityQueue<Path>();
-		pq.add(new Path(start, 0, start));
+		pq.add(new Path(start.getSymbol(), 0, start.getSymbol()));
 		ArrayList<String> visited = new ArrayList<String>();
-		
+
 		while(!pq.isEmpty()) {
 			Path nextEntry = pq.remove();
 			if(nextEntry.getVertex().equals(end)) {
 				return nextEntry;
-			} else if(visited.contains(nextEntry.getVertex()) {
-			   continue;
+			} else if(visited.contains(nextEntry.getVertex())) {
+				continue;
 			} else {
-				currVertex = nextEntry.getVertex();
-				currCost = nextEntry.getCost();
-				currPath = nextEntry.getPathStr();
+				String currVertex = nextEntry.getVertex();
+				int currCost = nextEntry.getCost();
+				String currPath = nextEntry.getPathStr();
 				visited.add(currVertex);
 				for(Edge e : graph.getGraph().get(currVertex)) {
-					String dest = e.getToVertex();
+					String dest = e.getToVertex().getSymbol();
 					if(!(visited.contains(dest))) {
-						nextCost = currCost + (useDistCost ? e.getDistanceCost() : e.getTimeCost());
-						nextPath = currPath + dest;
+						int nextCost = currCost + (Graph.useDistCost ? e.getDistanceCost() : e.getTimeCost());
+						String nextPath = currPath + dest;
 						pq.add(new Path(dest, nextCost, nextPath));
 					}
 				}		
 			}  
 		}
-		
+
 		return null;
 	}
-  
+
 }
