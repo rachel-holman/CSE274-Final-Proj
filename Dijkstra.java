84578b86e74b28d53a4526d551871fa6c1ba032c
ryanjamesyu <104389599+ryanjamesyu@users.noreply.github.com>
2022-04-28 17:57:42 -0400

    Update Dijkstra.java

--- Dijkstra.java
+++ Dijkstra.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 
@@ -28,7 +27,7 @@ public class Dijkstra {
 				Vertex currVertex = nextEntry.getVertex();
 				for(Edge e : graph.getGraph().get(currVertex)) {
 					if(!visited.contains(e.getToVertex())) {
-						LinkedList<Edge> newEdges = nextEntry.getEdges();
+						LinkedList<Edge> newEdges = new LinkedList<Edge>(nextEntry.getEdges());
 						newEdges.add(e);
 						int newCost = totalCost + (Graph.useDistCost ? e.getDistanceCost() : e.getTimeCost());
 						pq.add(new Path(graph.getVertex(e.getToVertex().getSymbol()), newCost, newEdges));
@@ -38,28 +37,6 @@ public class Dijkstra {
 			
 			visited.add(nextEntry.getVertex());
 		}
-		
-//		while(!pq.isEmpty()) {
-//			Path nextEntry = pq.remove();
-//			totalCost = nextEntry.getCost();
-//			if(nextEntry.getVertex().equals(end)) {
-//				return nextEntry;
-//			} else if(visited.contains(nextEntry.getVertex())) {
-//				continue;
-//			} else {
-//				Vertex currVertex = nextEntry.getVertex();
-//				LinkedList<Edge> path = nextEntry.getEdges();
-//				for(Edge e : graph.getGraph().get(currVertex)) {
-//					Vertex dest = e.getToVertex();
-//					if(!(visited.contains(dest))) {
-//						int newCost = totalCost + (Graph.useDistCost ? e.getDistanceCost() : e.getTimeCost());
-//						path.add(e);
-//						pq.add(new Path(dest, newCost, path));
-//					}
-//				}
-//				visited.add(currVertex);
-//			}  
-//		}
 
 		return null;
 	}
