78cc7b8fee39f6a5a67d4eb8fce037046fecf2d0
ryanjamesyu <104389599+ryanjamesyu@users.noreply.github.com>
2022-04-28 11:03:32 -0400

    Update Vertex.java

--- Vertex.java
+++ Vertex.java
@@ -1,19 +1,19 @@
 
+
 public class Vertex {
-  private String symbol;
-  private String address;
-  // private int x, y;
-  
-  public Vertex(String symbol, String address) {
+	private String symbol;
+	private String address;
+
+	public Vertex(String symbol, String address) {
 		this.symbol = symbol;
 		this.address = address;
 	}
-  
-  public String getSymbol() {
-    return this.symbol; 
-  }
-  
-  public String getAddress() {
-    return this.address; 
-  }
+
+	public String getSymbol() {
+		return this.symbol; 
+	}
+
+	public String getAddress() {
+		return this.address; 
+	}
 }
