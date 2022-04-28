55a17d363edcf4dd0f6524f9e9036885c16a3f54
ryanjamesyu <104389599+ryanjamesyu@users.noreply.github.com>
2022-04-28 11:04:29 -0400

    Update Edge.java

--- Edge.java
+++ Edge.java
@@ -1,28 +1,28 @@
 
 public class Edge {
-  private Vertex fromVertex, toVertex;
-  private int timeCost, distanceCost;
-  
-  public Edge(Vertex fromVertex, Vertex toVertex, int timeCost, int distanceCost) {
-    this.fromVertex = fromVertex;
-    this.toVertex = toVertex;
-    this.timeCost = timeCost;
-    this.distanceCost = distanceCost;
-  }
-  
-  public Vertex getFromVertex() {
-    return this.fromVertex; 
-  }
-  
-  public Vertex getToVertex() {
-    return this.toVertex; 
-  }
-  
-  public int getTimeCost() {
-    return this.timeCost; 
-  }
-  
-  public int getDistanceCost() {
-    return this.distanceCost; 
-  }
+	private Vertex fromVertex, toVertex;
+	private int timeCost, distanceCost;
+
+	public Edge(Vertex fromVertex, Vertex toVertex, int timeCost, int distanceCost) {
+		this.fromVertex = fromVertex;
+		this.toVertex = toVertex;
+		this.timeCost = timeCost;
+		this.distanceCost = distanceCost;
+	}
+
+	public Vertex getFromVertex() {
+		return this.fromVertex; 
+	}
+
+	public Vertex getToVertex() {
+		return this.toVertex; 
+	}
+
+	public int getTimeCost() {
+		return this.timeCost; 
+	}
+
+	public int getDistanceCost() {
+		return this.distanceCost; 
+	}
 }
