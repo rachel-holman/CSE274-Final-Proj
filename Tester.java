7b36080207936e37ea2e5c88362c858603fefc16
ryanjamesyu <104389599+ryanjamesyu@users.noreply.github.com>
2022-04-28 20:09:45 -0400

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,9 +1,20 @@
 
 public class Tester {
 	public static void main(String[] args) {
-//		new Window_GPS(new Graph("MapInformationXY.txt"));
 		Graph g = new Graph("MapInformation.txt");
 		// System.out.println(g);
-		System.out.println(Dijkstra.shortestPath(g, g.getVertex("A"), g.getVertex("B")));
+		System.out.println(Dijkstra.shortestPath(g, g.getVertex("Q"), g.getVertex("D")));
+		System.out.println();
+		Graph.useDistCost = true;
+		System.out.println(Dijkstra.shortestPath(g, g.getVertex("Q"), g.getVertex("D")));
+		System.out.println();
+		
+		Graph.returnAddress = true;
+		Graph.useDistCost = false;
+		System.out.println(Dijkstra.shortestPath(g, g.getVertex("Q"), g.getVertex("D")));
+		System.out.println();
+		Graph.useDistCost = true;
+		System.out.println(Dijkstra.shortestPath(g, g.getVertex("Q"), g.getVertex("D")));
+		System.out.println();
 	}
 }
