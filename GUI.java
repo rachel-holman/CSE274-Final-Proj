181ee896c56ac40ab0064b97b40b012ffa419648
Rachel Holman <78620569+rachel-holman@users.noreply.github.com>
2022-05-03 13:53:46 -0400

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,13 +1,16 @@
+/**
+ * 
+ * @author Rachel Holman, Ryan Yu, Ben Collinson
+ * @description: CSE 247 Final Project to create a program similar to a GPS system
+ * 
+ */
 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
-
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Scanner;
 
 
@@ -15,15 +18,15 @@ import java.util.Scanner;
 
 public class GUI extends JFrame implements ActionListener{
 
-	private static final int WIDTH = 1200;
-	private static final int HEIGHT = 640;
+	private static final int WIDTH = 1220;
+	private static final int HEIGHT = 740;
 	private ImageIcon imageIcon;
-	
+
 	Graph g = new Graph("MapInformation.txt");
 	JLabel label1, label2;
 	JButton compute;
 	JTextArea directions, placeholder;
-	JComboBox selectBox;
+	Vertex startV, endV;
 
 	/**
 	 * Constructs the GUI
@@ -32,160 +35,165 @@ public class GUI extends JFrame implements ActionListener{
 		setSize(WIDTH, HEIGHT);
 		setTitle("Shortest Path GPS");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		setLayout(new FlowLayout());
-		
+
 		//canvas = new DrawingCanvas();
 		//add(canvas);
-		
+
 		Container container = getContentPane();
 		container.setLayout(new BorderLayout());
 
 		//-------------------------------------------------------------- Right side of screen
-		
+
 		JPanel picPanel = new JPanel();
 		picPanel.setLayout(new FlowLayout());
-		
+
 		imageIcon = new ImageIcon(getClass().getResource("FinalProjectGraph_Final.png"));
 		Image image = imageIcon.getImage();
-		Image newimg = image.getScaledInstance(600, 600,  java.awt.Image.SCALE_SMOOTH); // scale image the smooth way  
+		Image newimg = image.getScaledInstance(620, 690,  java.awt.Image.SCALE_SMOOTH); // scale image the smooth way  
 		imageIcon = new ImageIcon(newimg);  // transform it back
-		
+
 		label1 = new JLabel(imageIcon);
 		picPanel.add(label1);			// adds graph picture to the gui
-		
-		
+
 		//-------------------------------------------------------------- Left side of screen
-		
+
 		JPanel info = new JPanel();
 		info.setLayout(new BorderLayout());
-		
+
 		JPanel select = select();
 		JPanel options = options();
 		JPanel output = output();
-		
+
 		info.add(select, BorderLayout.NORTH);
 		info.add(options, BorderLayout.CENTER);
 		info.add(output, BorderLayout.SOUTH);
-		
+
 		container.add(info, BorderLayout.WEST);
 		container.add(picPanel, BorderLayout.EAST);
 	}
+
 	
 	/**
 	 * Selection panel where user selects a starting vertex and a destination
-	 * @return
+	 * @return JPanel
 	 */
 	public JPanel select() {
 		JPanel select = new JPanel();
 		select.setLayout(new GridLayout(1,2));
-		
+
 		JPanel selectStart = new JPanel();
 		JPanel selectEnd = new JPanel();
-		
+
 		selectStart.setBorder(new TitledBorder("Starting Location"));
 		selectEnd.setBorder(new TitledBorder("Destination"));
+
 		String[] toAdd = new String[20];
 		try {
-		// Creates a scanner
-		Scanner file = new Scanner(new File("MapInformation.txt"));
-		String line = file.nextLine();
-		while (!line.equals("<Nodes>")) { line = file.nextLine(); }
+			// Creates a scanner
+			Scanner file = new Scanner(new File("MapInformation.txt"));
+			String line = file.nextLine();
+			while (!line.equals("<Nodes>")) { line = file.nextLine(); }
 
-					// Skips two lines of header text in the file
-		file.nextLine();
-		line = file.nextLine();
-		int i = 0;
-					// Creates Vertex objects (each of which contains a symbol and an address property)
-		while (!line.equals("</Nodes>")) {
+			// Skips two lines of header text in the file
+			file.nextLine();
+			line = file.nextLine();
+			int i = 0;
+			// Creates Vertex objects (each of which contains a symbol and an address property)
+			while (!line.equals("</Nodes>")) {
 				toAdd[i] = line;
 				line = file.nextLine();
 				i++;
-		}
+			}
 		} catch (FileNotFoundException e) {
-			
+
 		}
 		
+		JList<String> startL = new JList<String>(toAdd);
+		JList<String> endL = new JList<String>(toAdd);
 		
-		JList startL = new JList(toAdd);
-		JList endL = new JList(toAdd);
+		
+		
 		//startL.addListSelectionListener(s);
 		selectStart.add(startL);
 		selectEnd.add(endL);
 		select.add(selectStart, BorderLayout.WEST);
 		select.add(selectEnd, BorderLayout.EAST);
-		
+
 		return select;
 	}
+
 	
 	/**
-	 * Options panel where user selects time/distance cost and presses compute button
+	 * Options panel where user selects time/distance cost,
+	 * selects how to display the directions, and presses compute button
 	 * @return JPanel
 	 */
 	public JPanel options() {
 		JPanel options = new JPanel();
 		options.setBorder(new TitledBorder("Options and Controls"));
 		options.setLayout(new GridLayout(2,4));
-		
-		
+
+
 		//Create the radio buttons.
-	    JRadioButton time = new JRadioButton("Quickest");
-	    time.setMnemonic(KeyEvent.VK_Q);
-	    time.setActionCommand("time");
-	    time.setHorizontalAlignment(SwingConstants.CENTER);
-	    time.setSelected(true);
+		JRadioButton time = new JRadioButton("Quickest");
+		time.setMnemonic(KeyEvent.VK_Q);
+		time.setActionCommand("time");
+		time.setHorizontalAlignment(SwingConstants.CENTER);
+		time.setSelected(true);
 
-	    JRadioButton distance = new JRadioButton("Shortest");
-	    distance.setMnemonic(KeyEvent.VK_D);
-	    distance.setActionCommand("dist");
+		JRadioButton distance = new JRadioButton("Shortest");
+		distance.setMnemonic(KeyEvent.VK_D);
+		distance.setActionCommand("dist");
 
-	    //Group the radio buttons.
-	    ButtonGroup group = new ButtonGroup();
-	    group.add(time);
-	    group.add(distance);
-	    
-	  //Create the radio buttons.
-	    JRadioButton symbol = new JRadioButton("Symbol");
-	    symbol.setMnemonic(KeyEvent.VK_S);
-	    symbol.setActionCommand("symbol");
-	    symbol.setHorizontalAlignment(SwingConstants.CENTER);
-	    symbol.setSelected(true);
+		//Group the radio buttons.
+		ButtonGroup group = new ButtonGroup();
+		group.add(time);
+		group.add(distance);
 
-	    JRadioButton address = new JRadioButton("Address");
-	    address.setMnemonic(KeyEvent.VK_A);
-	    address.setActionCommand("address");
+		//Create the radio buttons.
+		JRadioButton symbol = new JRadioButton("Symbol");
+		symbol.setMnemonic(KeyEvent.VK_S);
+		symbol.setActionCommand("symbol");
+		symbol.setHorizontalAlignment(SwingConstants.CENTER);
+		symbol.setSelected(true);
 
-	    //Group the radio buttons.
-	    ButtonGroup group2 = new ButtonGroup();
-	    group2.add(symbol);
-	    group2.add(address);
-	    
-	    //Register a listener for the radio buttons.
-	    time.addActionListener(this);
-	    distance.addActionListener(this);
-	    address.addActionListener(this);
-	    symbol.addActionListener(this);
-	    
-	    //Create button to display directions in JTestArea
-	    compute = new JButton("Find path");
-	    compute.setActionCommand("compute");
-	    compute.addActionListener(this);
-	    
-	    //Create labels to make more user-friendly
-	    JLabel lab1 = new JLabel("Path type:", SwingConstants.RIGHT);
-	    JLabel lab2 = new JLabel("Directions output:", SwingConstants.RIGHT);
-	    
-	    options.add(lab1,0);
-	    options.add(time,1);
-	    options.add(distance,2);
-	    options.add(compute,3);
-	    options.add(lab2,4);
-	    options.add(symbol,5);
-	    options.add(address,6);
-		
+		JRadioButton address = new JRadioButton("Address");
+		address.setMnemonic(KeyEvent.VK_A);
+		address.setActionCommand("address");
+
+		//Group the radio buttons.
+		ButtonGroup group2 = new ButtonGroup();
+		group2.add(symbol);
+		group2.add(address);
+
+		//Register a listener for the radio buttons.
+		time.addActionListener(this);
+		distance.addActionListener(this);
+		address.addActionListener(this);
+		symbol.addActionListener(this);
+
+		//Create button to display directions in JTestArea
+		compute = new JButton("Find path");
+		compute.setActionCommand("compute");
+		compute.addActionListener(this);
+
+		//Create labels to make more user-friendly
+		JLabel lab1 = new JLabel("Path type:", SwingConstants.RIGHT);
+		JLabel lab2 = new JLabel("Directions output:", SwingConstants.RIGHT);
+
+		options.add(lab1,0);
+		options.add(time,1);
+		options.add(distance,2);
+		options.add(compute,3);
+		options.add(lab2,4);
+		options.add(symbol,5);
+		options.add(address,6);
+
 		return options;
-		
 	}
+
 	
 	/**
 	 * Output panel which displays shortest path
@@ -203,13 +211,12 @@ public class GUI extends JFrame implements ActionListener{
 
 		return output;
 	}
-	
+
 	/**
-	 * Action listener for the compute button
+	 * Action listener for all buttons. This method changes the useDistCost and
+	 * returnAddress variable based on user selection. When the "Find path" button is
+	 * pressed, this method prints the directions output in the JTextArea
 	 */
-	
-	// This method below will be used to change useDistCost from false to true with
-		// the press of a button
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getActionCommand().equals("time"))
@@ -221,21 +228,18 @@ public class GUI extends JFrame implements ActionListener{
 		else if (e.getActionCommand().equals("address"))
 			Graph.returnAddress = true;
 		else if (e.getActionCommand().equals("compute")) {
-			
-			// change path based on the two vertexes selected by user!!
-			Path p = Dijkstra.shortestPath(g, g.getVertex("Q"), g.getVertex("D"));
-			String s = p.toString();
-			directions.setText(s);
+			if (startV == null || endV == null)
+				directions.setText("");
+			else {
+				Path p = Dijkstra.shortestPath(g, startV, endV);
+				String s = p.toString();
+				directions.setText(s);
+			}
 		}
-			
-		
-		
-		
-	}
-	
-	
 
-	
+	}
+
+
 	/**
 	 * Main method
 	 * @param args
@@ -244,7 +248,5 @@ public class GUI extends JFrame implements ActionListener{
 		JFrame graph = new GUI();
 		graph.setVisible(true);
 	}
-	
-	
-	
-} 
+
+}
