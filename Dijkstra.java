83164a1571dfeebeedc22fc4ed3c9ab87b167451
ryanjamesyu <104389599+ryanjamesyu@users.noreply.github.com>
2022-05-03 14:55:27 -0400

    Update Dijkstra.java

--- Dijkstra.java
+++ Dijkstra.java
@@ -6,7 +6,6 @@ public class Dijkstra {
 
 	private static int totalCost;
 
-	// definitely not confident that this method actually works
 	public static Path shortestPath(Graph graph, Vertex start, Vertex end) {
 		
 		PriorityQueue<Path> pq = new PriorityQueue<Path>();
@@ -15,15 +14,20 @@ public class Dijkstra {
 		ArrayList<Vertex> visited = new ArrayList<Vertex>();
 
 		while(!pq.isEmpty()) {
+			
+			// get highest priority path and cost
 			Path nextEntry = pq.remove();
 			totalCost = nextEntry.getCost();
 			
+			// if we are at the ending vertex, end and return
 			if(nextEntry.getVertex() == end)
 				return nextEntry;
 			
+			// if the vertex has already been visited before, continue
 			if(visited.contains(nextEntry.getVertex())) {
 				continue;
 			} else {
+				// for each unvisited vertex, make new path and add it the the priority queue
 				Vertex currVertex = nextEntry.getVertex();
 				for(Edge e : graph.getGraph().get(currVertex)) {
 					if(!visited.contains(e.getToVertex())) {
@@ -35,9 +39,11 @@ public class Dijkstra {
 				}
 			}
 			
+			// add the current vertex to the list of visited vertices
 			visited.add(nextEntry.getVertex());
 		}
 
+		// if no path is found, return null
 		return null;
 	}
 
