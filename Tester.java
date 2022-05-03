080a6affb01e9ce1d5814446ff996c28540e64c3
Rachel Holman <78620569+rachel-holman@users.noreply.github.com>
2022-05-03 14:08:39 -0400

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,14 +1,25 @@
+/**
+ * 
+ * @author Rachel Holman, Ryan Yu, Ben Collinson
+ * @description: CSE 247 Final Project to create a program similar to a GPS system
+ * 
+ */
 
 public class Tester {
+	
 	public static void main(String[] args) {
+
 		Graph g = new Graph("MapInformation.txt");
-		// System.out.println(g);
+
+//		System.out.println(Dijkstra.shortestPath(g, g.getVertex("A"), g.getVertex("B")));
+//		System.out.println();
+		
 		System.out.println(Dijkstra.shortestPath(g, g.getVertex("Q"), g.getVertex("D")));
 		System.out.println();
 		Graph.useDistCost = true;
 		System.out.println(Dijkstra.shortestPath(g, g.getVertex("Q"), g.getVertex("D")));
 		System.out.println();
-		
+
 		Graph.returnAddress = true;
 		Graph.useDistCost = false;
 		System.out.println(Dijkstra.shortestPath(g, g.getVertex("Q"), g.getVertex("D")));
